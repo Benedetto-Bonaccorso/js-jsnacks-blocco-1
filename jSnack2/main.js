da5a7d04abee80c03610e6eb15c6ffe2f22e1ca8
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-12 12:10:20 +0200

    aggiunta manipolazione DOM

--- jSnack2/main.js
+++ jSnack2/main.js
@@ -1,8 +1,16 @@
 let word1 = prompt("insert a word");
 let word2 = prompt("insert another word");
 
+let h1 = document.querySelector("#heading")
+
+let longestWord;
+
 if(word1.length > word2.length){
     console.log(word1)
+    longestWord = word1
 } else {
     console.log(word2)
+    longestWord = word2
 }
+
+h1.innerHTML = longestWord
