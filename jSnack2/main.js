c4437875e46ff74b8578d3f68fd244fbede29813
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-12 12:24:16 +0200

    snack3

--- jSnack2/main.js
+++ jSnack2/main.js
@@ -9,10 +9,12 @@ if(word1.length > word2.length){
     console.log(word1,word2)
     longestWord = word1
     shortestWord = word2
-} else {
+} else if(word1.length > word2.length) {
     console.log(word2,word1)
     longestWord = word2
     shortestWord = word1
+} else{
+    h1.innerHTML = "The two words are of equal length"
 }
 
 h1.innerHTML = longestWord + " " + shortestWord
---
+++ jSnack3/main.js
@@ -0,0 +1,12 @@
+let number1 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number2 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number3 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number4 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number5 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number6 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number7 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number8 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number9 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+let number10 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
+
+console.log(number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10)
