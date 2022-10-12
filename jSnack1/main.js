f968ad62a6054732b7ea19b521d3b22a86de9461
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-12 11:44:19 +0200

    aggiunta in DOM

--- jSnack1/main.js
+++ jSnack1/main.js
@@ -1,8 +1,14 @@
 let numero1 = prompt("Inserisci un numero");
 let numero2 = prompt("Inserisci un altro numero");
 
+let h1 = document.querySelector("#heading")
+
+console.log(h1)
+
 if(numero1 > numero2){
     console.log(numero1)
+    h1.innerHTML = numero1
 } else {
     console.log(numero2)
+    h1.innerHTML = numero2
 }
