d44f432d14d0b94b4ba42f6ccfb32e03bfce607c
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-12 12:28:04 +0200

    DOM manipulation and error catching

--- jSnack3/main.js
+++ jSnack3/main.js
@@ -1,3 +1,5 @@
+let h1 = document.querySelector("#heading")
+
 let number1 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
 let number2 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
 let number3 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
@@ -9,4 +11,15 @@ let number8 = Number(prompt("Insert a number (you will need to imput 10 numbers)
 let number9 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
 let number10 = Number(prompt("Insert a number (you will need to imput 10 numbers)"));
 
-console.log(number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10)
+let finalNumber = number1 + number2 + number3 + number4 + number5 + number6 + number7 + number8 + number9 + number10
+
+let errorMessage = "one or more of the number inserted isn't valid"
+
+if(isNaN(finalNumber)){
+    alert(errorMessage)
+    h1.innerHTML = errorMessage
+    console.log(errorMessage)
+} else {
+    console.log(finalNumber)
+    h1.innerHTML = finalNumber
+}
