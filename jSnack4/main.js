d3cbe2aeccd67785d8ef91704c6ba82a1f3c752e
Benedetto-Bonaccorso <BenedettoBonaccorso00@gmail.com>
2022-10-13 12:36:50 +0200

    utilizzo ciclo for e markup in pagina

--- jSnack4/main.js
+++ jSnack4/main.js
@@ -4,8 +4,29 @@ let names = ["Marceline", "Finn", "Jake", "IceKing"]
 
 let userName = prompt("Insert your name to check if you're allowed inside")
 
+let allowed = false
+/*
 if(names.includes(userName)){
     alert("You're allowed")
 } else{
     alert("you're denied")
 }
+*/
+//Usando un ciclo
+
+for(let i = 0; i < names.length; i++){
+
+    if(userName == names[i]){
+        allowed = true
+    }
+
+    if(allowed == true){
+        alert("You're allowed")
+        h1.innerHTML = "You're allowed"
+        break
+    } else if (allowed == false && i == names.length-1){
+        alert("You're not allowed")
+        h1.innerHTML = "You're not allowed"
+    }
+}
+
